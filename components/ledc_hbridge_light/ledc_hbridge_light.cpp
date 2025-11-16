9aceedbb624d6ff4b3f22ffebc428a2ae49e8aed
Dimitry <dima.zhemkov@gmail.com>
2025-11-16 20:42:43 +0100

    Improve frequency handling and state adjustment in LedcHbridgeLightOutput

--- components/ledc_hbridge_light/ledc_hbridge_light.cpp
+++ components/ledc_hbridge_light/ledc_hbridge_light.cpp
@@ -25,16 +25,19 @@ void LedcHbridgeLightOutput::write_state(light::LightState *state) {
   cwhite = this->adjust_state(cwhite, frequency, this->cold_white_->get_max_power(), this->cold_white_->get_min_power());
   wwhite = this->adjust_state(wwhite, frequency, this->warm_white_->get_max_power(), this->warm_white_->get_min_power());
 
-  this->cold_white_->set_level(cwhite);
-  this->warm_white_->set_level(wwhite);
-
   if (frequency != this->frequency_)
   {
+    this->cold_white_->set_level(0);
+    this->warm_white_->set_level(0);
+
     ESP_LOGD(TAG, "Frequency changed: %.0f Hz -> %.0f Hz", this->frequency_, frequency);
     this->cold_white_->update_frequency(frequency);
     this->warm_white_->update_frequency(frequency);
     this->frequency_ = frequency;
   }
+
+  this->cold_white_->set_level(cwhite);
+  this->warm_white_->set_level(wwhite);
 }
 
 float LedcHbridgeLightOutput::calculate_frequency(float state, float max_power, float min_power) {
@@ -44,19 +47,10 @@ float LedcHbridgeLightOutput::calculate_frequency(float state, float max_power, 
   float min_duty_cycle = this->min_pulse_ / max_period;
   
   float frequency;
-  if (real_duty_cycle >= min_duty_cycle) {
+  if (real_duty_cycle >= min_duty_cycle || this->min_frequency_ == 0.0f) {
     frequency = this->max_frequency_;
-  } else if (real_duty_cycle > 0.0f) {
-    // float required_period = this->min_pulse_ / real_duty_cycle;
-    // frequency = 1.0f / required_period;
-    
-    // if (frequency < this->min_frequency_)
-    //   frequency = this->min_frequency_;
-    // else if (frequency > this->max_frequency_)
-    //   frequency = this->max_frequency_;
-    frequency = this->min_frequency_;
   } else {
-    frequency = this->max_frequency_;
+    frequency = this->min_frequency_;
   }
   
   return frequency;
@@ -69,12 +63,18 @@ float LedcHbridgeLightOutput::adjust_state(float state, float frequency, float m
   float min_duty_cycle = this->min_pulse_ / max_period;
   
   float corrected_duty_cycle;
-  if (real_duty_cycle >= min_duty_cycle) {
+  if (frequency == this->max_frequency_) {
     corrected_duty_cycle = state;
-  } else if (real_duty_cycle > 0.0f) {
-    corrected_duty_cycle = state * this->low_freq_brightness_correction_;
   } else {
-    corrected_duty_cycle = 0.0f;
+    corrected_duty_cycle = state * this->low_freq_brightness_correction_;
+    
+    float corrected_real_duty_cycle = get_real_duty_cycle(corrected_duty_cycle, max_power, min_power);
+    float period = 1.0f / frequency;
+    float min_duty_cycle_for_freq = this->min_pulse_ / period;
+    
+    if (corrected_real_duty_cycle > 0.0f && corrected_real_duty_cycle < min_duty_cycle_for_freq) {
+      corrected_duty_cycle = get_unscaled_duty_cycle(min_duty_cycle_for_freq, max_power, min_power);
+    }
   }
   
   return corrected_duty_cycle;
